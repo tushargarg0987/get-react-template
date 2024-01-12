d7388aac6b9cab8a0604fda17d5bee4437362c75
Tushar <tushargarg0987@gmail.com>
2024-01-12 17:41:11 +0530

    handling arguments

--- bin.js
+++ bin.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+
+import yargs from "yargs";
+import { hideBin } from 'yargs/helpers'
+
+async function main(args) {
+    if (args.argv._.length <= 1) {
+        console.log("Let's go!")
+    } else {
+        console.error(`\nerror: unexpected arguments\nusage: npx get-react-template <project name>:optional\n`)
+    }
+}
+const args = yargs(hideBin(process.argv));
+main(args);
