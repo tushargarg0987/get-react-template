441aeb1a24d9923dace2ab65c5c6c2ecd7cf03be
Tushar <tushargarg0987@gmail.com>
2024-01-12 17:54:12 +0530

    moulding fetched data

--- bin.js
+++ bin.js
@@ -7,6 +7,14 @@ import { fetchTemplates } from "./helper";
 
 async function main(args) {
     const data = await fetchTemplates();
+    const mapData = [];
+    data.map((ele) => {
+        mapData.push({
+            name: ele.name,
+            value: { git: ele.repo, repoName: ele.repoName, name: ele.name, script: ele.script },
+            description: `\n\x1b[0mDescription: \x1b[1m\x1b[4m${ele.description} \n\x1b[0mLive Preview : \x1b[1m\x1b[34m${ele.livePreview} \n\x1b[0mGithub Repository: \x1b[1m\x1b[34m${ele.repo.split('.git')[0]} \n\x1b[0mOwner: \x1b[1m\x1b[4m${ele.author.name} \n\x1b[0mThanks to \x1b[1m\x1b[34m${ele.author.github} \x1b[0m`
+        })
+    })
     if (args.argv._.length <= 1) {
         const answer = await select({
             message: 'Select a react theme',
