41d860f3912ed184a7e45fd33b6e1055028ac9c8
Tushar Garg <99884634+tushargarg0987@users.noreply.github.com>
2024-01-12 23:33:21 +0530

    Delete helper.js

--- helper.js
+++
@@ -1,9 +0,0 @@
-export async function fetchTemplates() {
-    var links;
-    await fetch('http://127.0.0.1:5500/data.json')
-        .then((response) => response.json())
-        .then((json) => {
-            links = json
-        });
-    return links;
-}
